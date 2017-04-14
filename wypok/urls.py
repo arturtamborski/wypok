c1b7068c6c8dd25a74fb16b0ea005fde7e98d7c3
Artur Tamborski <tamborskiartur@gmail.com>
2017-04-14 14:57:18 +0200

    add home app

--- wypok/urls.py
+++ wypok/urls.py
@@ -4,5 +4,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
-    url(r'^accounts/', include('allauth.urls')),
+    url(r'^', include('allauth.urls')),
+    url(r'^', include('home.urls')),
 ]
