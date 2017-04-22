fe3245a825a57df1822f6ab9941c7e82de0ba995
Artur Tamborski <tamborskiartur@gmail.com>
2017-04-22 16:51:48 +0200

    add simple account profile view and template

--- accounts/views.py
+++ accounts/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.urls import reverse
 
 def home(response):
-    return render(response, 'account/home.html')
+    return render(response, 'accounts/home.html')
+
+def profile(response, profile):
+    return render(response, 'accounts/profile.html')
