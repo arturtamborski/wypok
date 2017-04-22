9859458df01805e556752c1eff6082ff5ea08ad2
Artur Tamborski <tamborskiartur@gmail.com>
2017-04-22 17:03:34 +0200

    change order of url patterns

--- wypok/urls.py
+++ wypok/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
-    url(r'^', include('allauth.urls')),
     url(r'^', include('home.urls')),
+    url(r'^', include('accounts.urls')),
+    url(r'^', include('allauth.urls')),
 ]
