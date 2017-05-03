38bff36f14701a1a68ff2c87ddb1480955713f5c
Artur Tamborski <tamborskiartur@gmail.com>
2017-05-03 20:14:53 +0200

    register excavation in admin panel, add ready() method in config for future signals

--- excavation/admin.py
+++ excavation/admin.py
@@ -1,1 +1,6 @@
 from django.contrib import admin
+from . import models
+
+@admin.register(models.Post)
+class PostAdmin(admin.ModelAdmin):
+    pass
--- excavation/apps.py
+++ excavation/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class ExcavationConfig(AppConfig):
     name = 'excavation'
+
+    def ready(self):
+        from . import signals
