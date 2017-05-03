020a2004d5d657ca3b1847396589ae7d3104be8c
Artur Tamborski <tamborskiartur@gmail.com>
2017-05-04 00:39:16 +0200

    create default config for accounts app

--- accounts/apps.py
+++ accounts/apps.py
@@ -4,5 +4,5 @@ from django.apps import AppConfig
 class AccountsConfig(AppConfig):
     name = 'accounts'
 
-    #def ready(self):
-    #    import .signals
+    def ready(self):
+        from . import signals
