7c93e93727d1128de14b203fe533c4ab1ab367f0
Artur Tamborski <tamborskiartur@gmail.com>
2017-05-04 01:10:41 +0200

    add import reverse

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,6 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
+from django.urls import reverse
 
 class Profile(models.Model):
     user = models.OneToOneField(User, unique=True, on_delete=models.CASCADE)
