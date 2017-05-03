6d2dabb1b4f8ea95cb76d6c0f76a37fbf4999518
Artur Tamborski <tamborskiartur@gmail.com>
2017-05-04 00:43:29 +0200

    update accounts urls and fix profile view.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 from . import views
+from . import apps
 
 
+app_name = apps.AccountsConfig.name
 urlpatterns = [
-    url(r'^accounts/(?P<profile>[a-zA-Z][a-zA-Z0-9_-]{3,19})/$', views.profile, name='account_profile'),
+    url(r'^@(?P<profile>[a-zA-Z][a-zA-Z0-9_-]{3,19})/$', views.profile, name='profile'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
-from django.urls import reverse
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
+from . import models
+
+
+#class ProfileList(generic.ListView):
+#    queryset            = models.Account.objects.all()
+#    template_name       = 'account/profile.html'
+#    context_object_name = 'profiles'
+#
+#
+#class ProfileDetail(generic.DetailView):
+#    model               = models.Account
+#    template_name       = 'blog/article.html'
+#    context_object_name = 'profiles'
 
 def profile(response, profile):
-    return render(response, 'account/profile.html')
+    profile = get_object_or_404(models.Profile.objects, user__username=profile)
+    return render(response, 'account/profile.html', {'profile': profile})
