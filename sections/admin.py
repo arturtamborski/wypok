efc35b73261ed4ea6512e7e104dff3cf554dcfe3
Artur Tamborski <tamborskiartur@gmail.com>
2017-05-11 23:57:50 +0200

    register Section model in admin panel

--- sections/admin.py
+++ sections/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from . import models
 
+
+@admin.register(models.Section)
+class SectionAdmin(admin.ModelAdmin):
+    pass
+
 @admin.register(models.Post)
 class PostAdmin(admin.ModelAdmin):
     pass
