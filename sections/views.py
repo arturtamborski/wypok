bfed753e15ed78bd7d6b605540a7432b6ed164b8
Artur Tamborski <tamborskiartur@gmail.com>
2017-05-17 17:38:27 +0200

    use new caching middleware, validate sections (in a naive, stupid way i guess)

--- sections/views.py
+++ sections/views.py
@@ -1,20 +1,32 @@
-from django.views.decorators.cache import cache_page
+from allauth.account.decorators import verified_email_required as login_required
 from django.conf import settings
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
+from django.http import HttpResponseNotFound, Http404
+from wypok.cache import mark_for_caching
 from . import models
 from . import forms
 
-@cache_page(settings.CACHE_TTL)
+def check_section(section):
+    if not models.Section.objects.section_exists(section):
+        raise Http404
+
+
+@mark_for_caching
 def home(request, section=None):
     if section is None:
         return redirect('sections:home', 'all')
+    check_section(section)
 
     section = models.Section.objects.get_section(section)
     posts   = models.Post.objects.get_posts(section)
     return render(request, 'sections/home.html', {'section': section, 'posts': posts})
 
+@login_required
+@mark_for_caching
 def new_post(request, section):
+    check_section(section)
+
     form = forms.PostForm()
     if request.method == 'POST':
         form = forms.PostForm(request.POST)
@@ -23,8 +35,10 @@ def new_post(request, section):
 
     return render(request, 'sections/new_post.html', {'form': form})
 
-@cache_page(settings.CACHE_TTL)
+@mark_for_caching
 def post(request, section, id, slug=None):
+    check_section(section)
+
     section = models.Section.objects.get_section(section)
     post    = models.Post.objects.get_post(id)
     if slug is None:
