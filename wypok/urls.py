e74bd31ea6780c9d86649f49aad9912cf9d57c95
Artur Tamborski <tamborskiartur@gmail.com>
2017-05-20 17:31:35 +0200

    allow serving media dir in devel mode

--- wypok/urls.py
+++ wypok/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
+from django.conf.urls.static import static
 from django.views.generic import TemplateView
 
 
@@ -18,4 +19,4 @@ if settings.DEBUG:
     import debug_toolbar
     urlpatterns += [
         url(r'^__debug__/', include(debug_toolbar.urls)),
-    ]
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
