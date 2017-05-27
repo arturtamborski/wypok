48130945b461e3af3a71d2b72100c39433ac8b65
Artur Tamborski <tamborskiartur@gmail.com>
2017-05-27 20:37:43 +0200

    extend decorator

--- wypok/decorators.py
+++ wypok/decorators.py
@@ -1,19 +1,38 @@
 from functools import wraps
 
 from django.core.exceptions import ImproperlyConfigured, PermissionDenied
+from django.shortcuts import get_object_or_404
 from django.conf import settings
 
 
 OWNERSHIP_REQUIRED_CALLABLE = getattr(settings, 'OWNERSHIP_REQUIRED_CALLABLE', 'is_owner')
 
-def ownership_required(model):
+def ownership_required(model, **querys):
     def decorator(func):
         def view(request, *args, **kwargs):
-            if hasattr(model, OWNERSHIP_REQUIRED_CALLABLE):
-                attr = getattr(model, OWNERSHIP_REQUIRED_CALLABLE)
-                if attr(model, request, **kwargs):
-                    return func(request, *args, **kwargs)
+            query = querys.copy()
+
+            for k, v in query.items():
+                if v in kwargs:
+                    query[k] = kwargs[v]
+
+            obj = get_object_or_404(model, **query)
+            kwargs[v] = obj
+
+            if not hasattr(model, OWNERSHIP_REQUIRED_CALLABLE):
+                raise ImproperlyConfigured('"%s" doesn\'t have requried callable' % model.__name__)
+
+            attr = getattr(model, OWNERSHIP_REQUIRED_CALLABLE)
+            if not callable(attr):
+                raise ImproperlyConfigured('"%s" attribute is not callable' % attr.__name__)
+
+            result = attr(obj, request)
+            if request.user.is_superuser:
+                result = True
+
+            if not result:
                 raise PermissionDenied
-            raise ImproperlyConfigured('Provided model "%s" doesn\'t have requried callable for checking ownership.' % model.__name__)
+
+            return func(request, *args, **kwargs)
         return wraps(func)(view)
     return decorator
