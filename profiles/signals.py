68ccc5df94a87a320e86d12ceb4ff2be33e79e7e
Artur Tamborski <tamborskiartur@gmail.com>
2017-05-28 14:08:04 +0200

    import Group model...

--- profiles/signals.py
+++ profiles/signals.py
@@ -1,5 +1,6 @@
 from django.dispatch import receiver
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
 from django.db.models.signals import post_save
 from django.conf import settings
 
