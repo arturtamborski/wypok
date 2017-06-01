3aacbbaf606814f4b3e9730e488d90fde6186200
Artur Tamborski <tamborskiartur@gmail.com>
2017-06-01 23:33:02 +0200

    fix posts inclusion tag by passing in context

--- posts/templatetags/posts.py
+++ posts/templatetags/posts.py
@@ -3,10 +3,10 @@ from django import template
 
 register = template.Library()
 
-@register.inclusion_tag('posts/snippets/listing.html')
-def render_posts(section):
+@register.inclusion_tag('posts/snippets/listing.html', takes_context=True)
+def render_posts(context, section):
     posts = section.post_set.all()
     return dict(
-        section = section,
+        request = context.request,
         posts = posts,
     )
